3b67642b460ef6163d3c6db1ef6ce33f04fefb2c
Tefymora <tefamm1920@gmail.com>
2022-06-15 21:02:30 -0600

    test
    test
    
    Co-Authored-By: alvarikok3 <107597713+alvarikok3@users.noreply.github.com>

---
+++ Lubry-fast.py
@@ -0,0 +1,3 @@
+correo_electronico=input("Ingrese su correo electronico \nuse la estructura usuario@host.com: ")
+lugar_de_residencia=input("Ingrese su lugar de residencia: ")
+preparación_academica=input("Ingrese su preparación academica: ")
